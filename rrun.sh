779b3d34fd84a5adfa8145a37172e790fe967dde
Aravind Mallapureddy <aravindmallapureddy@Juspays-iMac.local>
2021-03-05 18:03:47 +0530

    added new files

---
+++ rrun.sh
@@ -0,0 +1,6 @@
+cp ./.vimrc ~/.vimrc
+cp ./.zshrc ~/.zshrc
+cp ./.tmux.conf ~/.tmux.conf 
+cp ./.chunkwmrc ~/.chunkwmrc
+cp ./.skhdrc ~/.skhdrc
+cp ./.config/wtf/config.yml ~/.config/wtf/config.yml 
