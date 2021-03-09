3de44292c76b1b7fed2f72e646178758b4a0abab
Aravind gopal mallapureddy <aravindgopal.ag@gmail.com>
2021-03-09 16:06:22 +0530

    updated with ghci

--- rrun.sh
+++ rrun.sh
@@ -1,6 +1,7 @@
 cp ./.vimrc ~/.vimrc
 cp ./.zshrc ~/.zshrc
 cp ./.tmux.conf ~/.tmux.conf 
+cp ./.ghci ~/.ghci
 cp ./.chunkwmrc ~/.chunkwmrc
 cp ./.skhdrc ~/.skhdrc
 cp ./.config/wtf/config.yml ~/.config/wtf/config.yml 
--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 cp ~/.vimrc ./
 cp ~/.zshrc ./
 cp ~/.tmux.conf ./
+cp ~/.ghci ./
 cp ~/.chunkwmrc ./
 cp ~/.skhdrc ./
 cp ~/.config/wtf/config.yml ./
